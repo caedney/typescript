59bccf72f8ffdd23a6a7722fd9c2fa7fffe00dcf
Craig Edney <craig@wearedauntless.com>
2019-10-14 17:26:51 +0100

    Added Cat component

---
+++ src/components/Cat/Cat.tsx
@@ -0,0 +1,26 @@
+import React, { FunctionComponent } from 'react';
+
+import { MouseState } from '../Mouse/Mouse';
+
+import catImg from "./cat.jpg";
+import styles from './Cat.module.scss';
+
+type CatProps = {
+  mouse: MouseState
+}
+
+const Cat: FunctionComponent<CatProps> = ({ mouse }) => {
+  return (
+    <img
+      className={styles.cat}
+      src={catImg}
+      alt="Cat"
+      style={{
+        left: mouse.x,
+        top: mouse.y
+      }} 
+    />
+  )
+}
+
+export default Cat;
