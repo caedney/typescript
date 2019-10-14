d6f3de8b745116d07f2b89e4707c538abee193e9
Craig Edney <craig@wearedauntless.com>
2019-10-14 17:27:25 +0100

    Added Search component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent, SyntheticEvent } from 'react';
+
+import Button from '../Button/Button';
+import Input from '../Input/Input';
+
+import styles from './Search.module.scss';
+
+const Search: FunctionComponent = () => {
+
+  const handleOnSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log('Search', e);
+  }
+
+  return (
+    <div className={styles.search}>
+      <form onSubmit={handleOnSubmit}>
+        <Input />
+        <Button>Submit</Button>
+      </form>
+    </div>
+  );
+}
+
+export default Search;
