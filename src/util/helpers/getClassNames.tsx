1ecb3736534f86072425f356999986f167ebc020
Craig Edney <craig@wearedauntless.com>
2019-10-17 16:38:23 +0100

    Added space to comment block

--- src/util/helpers/getClassNames.tsx
+++ src/util/helpers/getClassNames.tsx
@@ -1,19 +1,18 @@
 /**
- * @param {Object} classNames - An object of classNames
- * @return {(string | undefined)} - Returns a class string or undefined
+ * @param { Object } classNames - An object of classNames
+ * @return {( string | undefined )} - Returns a class string or undefined
  */
 
 type TypeProps = {
-  [key: string]: boolean
-}
+  [key: string]: boolean;
+};
 
 function getClassNames(classNames: TypeProps): string | undefined {
-
   const classes = Object.keys(classNames)
     .filter(key => classNames[key])
     .join(' ');
 
   return classes.length > 0 ? classes : undefined;
-};
+}
 
 export default getClassNames;
