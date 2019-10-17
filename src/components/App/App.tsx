b026765e299491d256459600d10d81b3f6e492f2
Craig Edney <craig@wearedauntless.com>
2019-10-17 16:21:34 +0100

    Added Context Provider to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,15 @@
+//----------------------------------------------------------------------------//
+//                            Imported NPM modules                            //
+//----------------------------------------------------------------------------//
+
 import React, { FunctionComponent } from 'react';
 
-import logo from './logo.svg';
+//----------------------------------------------------------------------------//
+//                        Imported app modules/assets                         //
+//----------------------------------------------------------------------------//
+
+import AppContext from '../../context/appContext';
+import Header from '../Header/Header';
 import Search from '../Search/Search';
 import Counter from '../Counter/Counter';
 import Graph from '../Graph/Graph';
@@ -9,21 +18,41 @@ import ExpansionPanel from '../ExpansionPanel/ExpansionPanel';
 
 import styles from './App.module.scss';
 
+//----------------------------------------------------------------------------//
+//                          Exported React component                          //
+//----------------------------------------------------------------------------//
+
 const App: FunctionComponent = () => {
   return (
-    <div className={styles.app}>
-      <header className={styles["app-header"]}>
-        <img src={logo} className={styles["app-logo"]} alt="logo" />
-      </header>
-      <Search />
-      <ExpansionPanel title="Expansion Panel">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus repellendus adipisci aspernatur molestias fuga, nobis aliquid molestiae perferendis aut assumenda blanditiis neque dolor delectus dignissimos explicabo quis amet error iste porro velit a distinctio placeat fugit! Amet tenetur cupiditate ipsum.</p>
-      </ExpansionPanel>
-      <Counter initialCount={8} />
-      <Graph onClick={() => { console.log('Graph') }} />
-      <MouseTracker />
-    </div>
+    <AppContext.Provider
+      value={{
+        authenticated: true,
+        lang: 'en',
+        theme: 'light',
+      }}
+    >
+      <div className={styles.app}>
+        <Header />
+        <Search />
+        <ExpansionPanel title={'Expansion Panel'}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+            repellendus adipisci aspernatur molestias fuga, nobis aliquid
+            molestiae perferendis aut assumenda blanditiis neque dolor delectus
+            dignissimos explicabo quis amet error iste porro velit a distinctio
+            placeat fugit! Amet tenetur cupiditate ipsum.
+          </p>
+        </ExpansionPanel>
+        <Counter initialCount={8} />
+        <Graph
+          onClick={() => {
+            console.log('Graph');
+          }}
+        />
+        <MouseTracker />
+      </div>
+    </AppContext.Provider>
   );
-}
+};
 
 export default App;
