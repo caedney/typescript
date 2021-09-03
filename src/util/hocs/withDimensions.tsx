612eaae2acf5036874dbf84ecd4bea7a398c5ba5
Craig Edney <caedney@btinternet.com>
2021-09-03 09:48:21 +0100

    Move props spread to beginning of list

--- src/util/hocs/withDimensions.tsx
+++ src/util/hocs/withDimensions.tsx
@@ -4,32 +4,31 @@ import React, {
   useRef,
   useState,
   useLayoutEffect,
-  useEffect
+  useEffect,
 } from 'react';
 import { Diff } from 'utility-types';
 
 export type WithDimensionsProps = {
-  width: number,
-  height: number,
-  forwardRef: RefObject<HTMLDivElement>
-}
+  width: number;
+  height: number;
+  forwardRef: RefObject<HTMLDivElement>;
+};
 
 function withDimensions<T extends WithDimensionsProps>(
   WrappedComponent: ComponentType<T>
 ) {
-
   return (props: Diff<T, WithDimensionsProps>) => {
     const ref = useRef<HTMLDivElement>(null);
 
     const [{ width, height }, setDimensions] = useState({
       width: 0,
-      height: 0
-    })
+      height: 0,
+    });
 
     function updateDimensions() {
       const { current } = ref;
 
-      if (current !== null){
+      if (current !== null) {
         setDimensions({
           width: current.offsetWidth,
           height: current.offsetHeight,
@@ -46,18 +45,18 @@ function withDimensions<T extends WithDimensionsProps>(
 
       return () => {
         window.removeEventListener('resize', updateDimensions);
-      }
-    })
+      };
+    });
 
     return (
       <WrappedComponent
+        {...(props as T)}
         width={width}
         height={height}
         forwardRef={ref}
-        {...props as T}
       />
-    )
+    );
   };
-};
+}
 
 export default withDimensions;
